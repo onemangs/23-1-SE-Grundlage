e19ac550b95ecff5d6258c656e1f2a75685e26ca
onemangs <144699125+onemangs@users.noreply.github.com>
2023-09-12 11:22:58 +0200

    init

---
+++ Schleifen/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Schleifen-Demo");
+int itercount = 10;
+while(itercount > 0)
+{
+  Console.WriteLine("MEEP");
+  itercount = itercount - 1;
+  //itercount--;
+  //--itercount; !! Achtung: Nicht das gleiche 
+}
+itercount = 10;
+do
+{
+  Console.WriteLine("DO - WHILE");
+  itercount--;
+
+  
+} while (itercount > 0);
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Willkommen beim Tascherechner 3000");
+bool CheckParse1 =false;
+bool CheckParse2 =false;
+var num1 = 0;
+var num2 = 0;
+
+while (!CheckParse1 || !CheckParse2)
+
+{
+  Console.WriteLine("bitte gebe Zahl 1 an:");
+  var input1 = Console.ReadLine();
+  Console.WriteLine("bitte gebe Zahl 2 an:");
+  var input2 = Console.ReadLine();
+
+  //Konvertierung von Text zu Zahlen
+  
+
+  CheckParse1 = int.TryParse(input1, out num1);
+  CheckParse2 = int.TryParse(input2, out num2);
+
+}
+
+
+var result = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {result}");
+
+
+
+
+
+
+//entweder (var), oder (datentyp eg. string, int, float, decimal...) 
+//string test1= "buxtehude"; 
